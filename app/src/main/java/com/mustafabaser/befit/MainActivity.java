6798e7fcb5ba6d45938f10a692bf1f5f75adc66b
MustafaBaser <32432625+MustafaBaser@users.noreply.github.com>
2020-01-01 00:54:05 +0300

    Activities

--- app/src/main/java/com/mustafabaser/befit/MainActivity.java
+++ app/src/main/java/com/mustafabaser/befit/MainActivity.java
@@ -2,13 +2,31 @@ package com.mustafabaser.befit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
     }
-}
+        public void programlar(View view){
+            Intent programsActivity = new Intent(MainActivity.this, Programlar.class);
+            startActivity(programsActivity);
+        }
+        public void ayarlar(View view){
+            Intent ayarlarActivity = new Intent(MainActivity.this,Ayarlar.class);
+            startActivity(ayarlarActivity);
+        }
+
+        public void antrenman(View view){
+            Intent antrenmanActivity = new Intent(MainActivity.this,Antrenman.class);
+            startActivity(antrenmanActivity);
+        }
+    }
+
---
+++ app/src/main/java/com/mustafabaser/befit/Programlar.java
@@ -0,0 +1,16 @@
+package com.mustafabaser.befit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class Programlar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_programlar);
+    }
+}
