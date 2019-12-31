cac88c52d69a5fb9876422d2f0d0f679fd8f1b42
MustafaBaser <32432625+MustafaBaser@users.noreply.github.com>
2019-12-31 16:05:07 +0300

    Splash Screen

---
+++ app/src/main/java/com/mustafabaser/befit/SplashActivity.java
@@ -0,0 +1,25 @@
+package com.mustafabaser.befit;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static final int SPLASH_TIME = 3500;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        },SPLASH_TIME);
+    }
+}
