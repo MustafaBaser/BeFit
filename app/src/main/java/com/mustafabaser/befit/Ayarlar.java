96597f704a7df600e2f907be9758931fcf809f05
MustafaBaser <32432625+MustafaBaser@users.noreply.github.com>
2020-01-02 15:01:11 +0300

    Update

--- app/src/main/java/com/mustafabaser/befit/Ayarlar.java
+++ app/src/main/java/com/mustafabaser/befit/Ayarlar.java
@@ -1,16 +1,21 @@
 package com.mustafabaser.befit;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.drawerlayout.widget.DrawerLayout;
 import android.os.Bundle;
 import android.view.WindowManager;
 
+
 public class Ayarlar extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_ayarlar);
+
+
     }
 }
--- app/src/main/java/com/mustafabaser/befit/MainActivity.java
+++ app/src/main/java/com/mustafabaser/befit/MainActivity.java
@@ -1,13 +1,13 @@
 package com.mustafabaser.befit;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,12 +21,16 @@ public class MainActivity extends AppCompatActivity {
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
+        String welcomeToast = "BeFit ile daha sağlıklı vücutlar!";
+
+        Toast.makeText(getApplicationContext(), welcomeToast , Toast.LENGTH_LONG).show();
+
         btnShare = (Button) findViewById(R.id.button4);
 
-        btnShare.setOnClickListener(new View.OnClickListener(){
+        btnShare.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view){
-                shareIntent = new Intent (Intent.ACTION_SEND);
+            public void onClick(View view) {
+                shareIntent = new Intent(Intent.ACTION_SEND);
                 shareIntent.setType("text/pain");
                 shareIntent.putExtra(Intent.EXTRA_TEXT, shareBody);
                 startActivity(Intent.createChooser(shareIntent, "Şununla paylaş: "));
@@ -34,21 +38,24 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        }
-        public void programlar(View view){
-            Intent programsActivity = new Intent(MainActivity.this, Programlar.class);
-            startActivity(programsActivity);
-        }
-        public void ayarlar(View view){
-            Intent ayarlarActivity = new Intent(MainActivity.this,Ayarlar.class);
-            startActivity(ayarlarActivity);
-        }
-
-        public void antrenman(View view){
-            Intent antrenmanActivity = new Intent(MainActivity.this,Antrenman.class);
-            startActivity(antrenmanActivity);
-        }
-
     }
+
+
+    public void programlar(View view) {
+        Intent programsActivity = new Intent(MainActivity.this, Programlar.class);
+        startActivity(programsActivity);
+    }
+
+    public void ayarlar(View view) {
+        Intent ayarlarActivity = new Intent(MainActivity.this, Ayarlar.class);
+        startActivity(ayarlarActivity);
+    }
+
+    public void antrenman(View view) {
+        Intent antrenmanActivity = new Intent(MainActivity.this, Antrenman.class);
+        startActivity(antrenmanActivity);
+    }
+
+}
 
 
