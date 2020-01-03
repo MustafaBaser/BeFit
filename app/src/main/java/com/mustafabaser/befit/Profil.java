a6db6dc5019246d71dc506ff25c01f39cb5af544
MustafaBaser <32432625+MustafaBaser@users.noreply.github.com>
2020-01-03 21:51:48 +0300

    SQLite Database

--- app/src/main/java/com/mustafabaser/befit/Profil.java
+++ app/src/main/java/com/mustafabaser/befit/Profil.java
@@ -3,17 +3,73 @@ package com.mustafabaser.befit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class Profil extends AppCompatActivity {
+
+    ListView listView;
+    ArrayList<String> nameArray;
+    ArrayList<Integer> idArray;
+    ArrayAdapter arrayAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profil);
+
+        listView = findViewById(R.id.listView);
+        nameArray = new ArrayList<>();
+        idArray = new ArrayList<>();
+
+        arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, nameArray);
+        listView.setAdapter(arrayAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                Intent intent = new Intent(Profil.this,ProfiliGuncelle.class);
+                intent.putExtra("id", idArray.get(i));
+                intent.putExtra("info", "old");
+                startActivity(intent);
+
+            }
+        });
+
+        getData();
+
+    }
+
+    public void getData() {
+
+        try {
+            SQLiteDatabase database = this.openOrCreateDatabase("KisiselBilgiler", MODE_PRIVATE, null);
+            Cursor cursor = database.rawQuery("SELECT * FROM kisiselbilgiler", null);
+            int nameIndex = cursor.getColumnIndex("name");
+            int idIndex = cursor.getColumnIndex("id");
+
+            while (cursor.moveToNext()) {
+                nameArray.add(cursor.getString(nameIndex));
+                idArray.add(cursor.getInt(idIndex));
+            }
+            arrayAdapter.notifyDataSetChanged();
+            cursor.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 
@@ -31,8 +87,9 @@ public class Profil extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
 
-        if(item.getItemId() == R.id.add_information_to_db){
-            Intent intent = new Intent(Profil.this,ProfiliGuncelle.class);
+        if (item.getItemId() == R.id.add_information_to_db) {
+            Intent intent = new Intent(Profil.this, ProfiliGuncelle.class);
+            intent.putExtra("info", "new");
             startActivity(intent);
         }
         return super.onOptionsItemSelected(item);
--- app/src/main/java/com/mustafabaser/befit/ProfiliGuncelle.java
+++ app/src/main/java/com/mustafabaser/befit/ProfiliGuncelle.java
@@ -9,7 +9,11 @@ import androidx.core.content.ContextCompat;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteStatement;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.ImageDecoder;
 import android.net.Uri;
 import android.os.Build;
@@ -30,6 +34,8 @@ public class ProfiliGuncelle extends AppCompatActivity {
     EditText name, age, gender, height, weight;
     Button button;
 
+    SQLiteDatabase database;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,24 +49,73 @@ public class ProfiliGuncelle extends AppCompatActivity {
         weight = findViewById(R.id.dbWeight);
         button = findViewById(R.id.buttonSave);
 
-        }
+        database = this.openOrCreateDatabase("KisiselBilgiler", MODE_PRIVATE, null);
 
-        public void selectImage(View view){
 
-            if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
-                ActivityCompat.requestPermissions(this,new String[] {Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
-            }else{
-                Intent intentToGallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-                startActivityForResult(intentToGallery,2);
+        Intent intent = getIntent();
+        String info = intent.getStringExtra("info");
+
+        if(info.matches("new")){
+
+            name.setText("");
+            age.setText("");
+            gender.setText("");
+            height.setText("");
+            weight.setText("");
+            button.setVisibility(View.VISIBLE);
+
+        }else{
+            int personId = intent.getIntExtra("id", 1);
+            button.setVisibility(View.INVISIBLE);
+
+
+            try{
+
+                Cursor cursor = database.rawQuery("SELECT * FROM kisiselbilgiler WHERE id = ?", new String[] {String.valueOf(personId)});
+
+                int nameIndex = cursor.getColumnIndex("name");
+                int ageIndex = cursor.getColumnIndex("age");
+                int genderIndex = cursor.getColumnIndex("gender");
+                int heightIndex = cursor.getColumnIndex("height");
+                int weightIndex = cursor.getColumnIndex("weight");
+                int imageIndex = cursor.getColumnIndex("image");
+
+                while (cursor.moveToNext()){
+                    name.setText(cursor.getString(nameIndex));
+                    age.setText(cursor.getString(ageIndex));
+                    gender.setText(cursor.getString(genderIndex));
+                    height.setText(cursor.getString(heightIndex));
+                    weight.setText(cursor.getString(weightIndex));
+                    byte[] bytes = cursor.getBlob(imageIndex);
+                    Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+                    imageView.setImageBitmap(bitmap);
+                }
+                cursor.close();
+
+
+        }catch (Exception e){
+
             }
+
         }
+    }
+
+    public void selectImage(View view) {
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+        } else {
+            Intent intentToGallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            startActivityForResult(intentToGallery, 2);
+        }
+    }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        if(requestCode == 1){
-            if(grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+        if (requestCode == 1) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 Intent intentToGallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-                startActivityForResult(intentToGallery,2);
+                startActivityForResult(intentToGallery, 2);
             }
         }
 
@@ -69,31 +124,31 @@ public class ProfiliGuncelle extends AppCompatActivity {
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        if(requestCode== 2 && resultCode == RESULT_OK && data != null) {
+        if (requestCode == 2 && resultCode == RESULT_OK && data != null) {
             Uri imageData = data.getData();
 
 
             try {
 
-                if(Build.VERSION.SDK_INT >= 28) {
+                if (Build.VERSION.SDK_INT >= 28) {
 
                     ImageDecoder.Source source = ImageDecoder.createSource(this.getContentResolver(), imageData);
                     selectedImage = ImageDecoder.decodeBitmap(source);
                     imageView.setImageBitmap(selectedImage);
 
-                }else {
+                } else {
 
                     selectedImage = MediaStore.Images.Media.getBitmap(this.getContentResolver(), imageData);
                     imageView.setImageBitmap(selectedImage);
-                    }
-                }catch (IOException e) {
+                }
+            } catch (IOException e) {
                 e.printStackTrace();
             }
         }
         super.onActivityResult(requestCode, resultCode, data);
     }
 
-    public void dbSave (View view){
+    public void dbSave(View view) {
 
         String dbName = name.getText().toString();
         String dbAge = age.getText().toString();
@@ -107,24 +162,44 @@ public class ProfiliGuncelle extends AppCompatActivity {
         smallImage.compress(Bitmap.CompressFormat.PNG, 50, outputStream);
         byte[] byteArray = outputStream.toByteArray();
 
+        try {
+            database = this.openOrCreateDatabase("KisiselBilgiler", MODE_PRIVATE, null);
+            database.execSQL("CREATE TABLE IF NOT EXISTS kisiselbilgiler (id INTEGER PRIMARY KEY, name VARCHAR, age VARCHAR, gender VARCHAR, height VARCHAR, weight VARCHAR, image BLOB)");
+
+            String sqlString = "INSERT INTO kisiselbilgiler (name, age, gender, height, weight, image) VALUES (?, ?, ?, ?, ?, ?)";
+            SQLiteStatement sqLiteStatement = database.compileStatement(sqlString);
+            sqLiteStatement.bindString(1, dbName);
+            sqLiteStatement.bindString(2, dbAge);
+            sqLiteStatement.bindString(3, dbGender);
+            sqLiteStatement.bindString(4, dbHeight);
+            sqLiteStatement.bindString(5, dbWeight);
+            sqLiteStatement.bindBlob(6, byteArray);
+            sqLiteStatement.execute();
+
+        } catch (Exception e) {
+            e.printStackTrace();
         }
 
-        public Bitmap makeSmallerImage(Bitmap image, int maximumSize){
+        Intent geriDon = new Intent(this, Profil.class);
+        geriDon.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(geriDon);
+    }
+
+    public Bitmap makeSmallerImage(Bitmap image, int maximumSize) {
         int width = image.getWidth();
         int height = image.getHeight();
 
-        float bitmapRatio= (float) width / (float) height;
+        float bitmapRatio = (float) width / (float) height;
 
-        if(bitmapRatio>1){
+        if (bitmapRatio > 1) {
             width = maximumSize;
-            height = (int) (width/bitmapRatio);
+            height = (int) (width / bitmapRatio);
 
-        }else {
+        } else {
             height = maximumSize;
             width = (int) (height * bitmapRatio);
         }
 
-        return Bitmap.createScaledBitmap(image,width,height,true);
-        }
-
+        return Bitmap.createScaledBitmap(image, width, height, true);
+    }
 }
